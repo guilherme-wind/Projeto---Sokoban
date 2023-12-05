38d1d4e8cb8abd0b1c8d76f52f753c9217ce1213
pedrocasc <pedrocascao03@gmail.com>
2023-12-05 17:57:58 +0000

    estamos num caminho (espero que bom)

--- sokobozo.py
+++ sokobozo.py
@@ -5,63 +5,52 @@ from search import *
 
 def sokoban(puzzle):
 
-    # Converte a grelha em uma matriz de objetos
+    # Converte a a string "puzzle" em uma matriz de chars
     m = puzzle.split('\n')[:-1]
     n = len(m)
     grid = []
-    print(m)
     for i in range(n):
         grid.append([obj for obj in m[i]])
 
-    # Obtém a posição do sokoban
-    sx, sy = None, None
+    sx, sy = None, None         # Posição do sokoban
+    caixas = []                 # Posições das caixas
+    objetivos = []              # Posições dos objetivos
     for i in range(n):
         for j in range(len(grid[i])):
             if grid[i][j] in ['@', '+']:
                 sx, sy = i, j
-                break
-
-    # Obtém as posições das caixas
-    caixas = []
-    for i in range(n):
-        for j in range(len(grid[i])):
             if grid[i][j] in ['$', '*']:
                 caixas.append((i, j))
-
-    # Obtém as posições dos objectivos
-    objetivos = []
-    for i in range(n):
-        for j in range(len(grid[i])):
             if grid[i][j] in ['o', '+', '*']:
                 objetivos.append((i, j))
 
-    # Constrói o estado inicial
-    estado = []
-    for i in range(n):
-        estado.append([expr('No(s)') if grid[i][j] == '@' else expr('Livre({})'.format(grid[i][j])) for j in range(len(grid[i]))])
+    estado = []                 # Estado inicial
+    estado.append(expr('Sokoban({})'.format((sx, sy))))
+    for i in range(len(caixas)):
+        estado.append(expr('Caixa({})'.format(caixas[i])))
 
-    # Constrói o objetivo
-    objetivo = []
-    for i in range(n):
-        objetivo.append(expr('No({})'.format(grid[i][j])) for j in range(len(grid[i])))
+    objetivo = []               # Objetivo
+    for i in range(len(objetivos)):
+        objetivo.append(expr('Caixa({})'.format(objetivos[i])))
 
-    # Constrói a instância do problema
-    p = PlanningProblem(initial=estado, goals=objetivo, actions=[], domain=[])
-
+    acoes = []
     # Gera as ações
     for caixa in caixas:
         for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            if 0 <= caixa[0] + dx < n and 0 <= caixa[1] + dy < len(grid[0]) and grid[caixa[0] + dx][caixa[1] + dy] == '.':
-                # Ação para mover a caixa para cima
-                a = Action('Move(C,{},{})'.format(caixa[0], caixa[1]),
-                             precond=expr('No({}) & No(C) & No(C,{},{})'.format(caixa[0] + dx, caixa[1] + dy, caixa[0], caixa[1])),
-                             effect=expr('No(C,{}) & No({}) & No({}) & No(C) & Sobre(C,{},{})'.format(caixa[0], caixa[1], caixa[0] + dx, caixa[1] + dy, caixa[0] + dx, caixa[1] + dy)))
-                p.add_action(a)
+            if 0 <= caixa[0] + dx < n and 0 <= caixa[1] + dy < len(grid[0]) and grid[caixa[0] + dx][caixa[1] + dy] != '#':
+                a = Action('Move(C,{},{})'.format(caixa[0] + dx, caixa[1] + dy),
+                             precond=expr('Caixa({})'.format((caixa[0], caixa[1]))),
+                             effect=expr('Caixa({})'.format((caixa[0] + dx, caixa[1] + dy))))
+                acoes.append(a)
 
-    # Guarda as ações expandidas
-    p.expanded_actions = p.actions
+    # Constrói a instância do problema
+    pp = PlanningProblem(initial=estado, goals=objetivo, actions=[], domain=[])         # parece-me bem até aqui
+    # as acoes é que estao provavelmente mal, nao sei como fazer
+    fp = ForwardPlan(pp)     # isto crasha o programa, provavelmente por causa das acoes
+    # fp = ForwardPlan(pp, True, True)
+    fp.expanded_actions = acoes
 
-    return p
+    return fp
 
 
 
